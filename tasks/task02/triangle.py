cd5dfce7419279938e1f147fc6c65429f2c0f144
shagovAlexei <shagovAlexei@gmail.com>
2021-09-07 22:14:37 +0300

    add def area_angle

--- tasks/task02/triangle.py
+++ tasks/task02/triangle.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+# Import the necessary packages
+from consolemenu import *
+from consolemenu.items import *
+import math
+
+def qwe(a, b):
+	# qwe(2, 3)
+	return print(a+b)
+
+def area_test(b, h):
+	a = 0.5 * float(b) * float(h)
+	return a
+
+
+def area_angle(a, b, angle):
+	ar = 0.5 * float(a) * float(b) * float(math.sin(math.radians(int(angle))))
+	print(ar)
+	return ar
+
+def main():
+	a = input("Input a: ")
+	b = input("Input b: ")
+	angle = input("Input alfa: ")
+	area_angle(a, b, angle)
+if __name__ == "__main__":
+    main()
