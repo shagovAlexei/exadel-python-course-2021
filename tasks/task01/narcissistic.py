d606da21e01ba09dd49fe56793927a61b02a0312
shagovAlexei <shagovAlexei@gmail.com>
2021-09-07 20:21:06 +0300

    edit narcissistic.py

--- tasks/task01/narcissistic.py
+++ tasks/task01/narcissistic.py
@@ -1,6 +1,6 @@
 # Shagov Aleksei
 # Function to count digit in number
-def count_digit(n):
+def count_tens(n):
     count = 0
     while n:
         n //= 10
@@ -9,14 +9,14 @@ def count_digit(n):
 
 
 # Returns true if n is Narcissistic number
-def check(n):
+def is_narcissistic_number(n):
 
     if n < 0:
         return False
     
     number_copy = n
     arm_sum = 0
-    digit = count_digit(n)
+    digit = count_tens(n)
     while n:
         remainder = n%10
         arm_sum += remainder**digit
@@ -26,5 +26,5 @@ def check(n):
 
 # Loop
 for i in range(1,1000):
-    if check(i):
+    if is_narcissistic_number(i):
         print(i, end=' ')
