c7e77587fa646b56d8318e46514ec1af6a88584c
shagovAlexei <shagovAlexei@gmail.com>
2021-09-17 15:57:42 +0300

    del tab

--- tasks/task02/triangle.py
+++ tasks/task02/triangle.py
@@ -25,53 +25,52 @@ Goodbye!
 import math
 
 def input_validation(input_string, item):
-		try:
-				value = abs(float(input_string.split()[item]))
-				if value == 0:
-					print("\nInvalid input data!")
-					main()
-				return value
-		except:
-				print("\nInvalid input data!")
-				main()
-		
+    try:
+        value = abs(float(input_string.split()[item]))
+        if value == 0:
+            print("\nInvalid input data!")
+            main()
+        return value
+    except:
+        print("\nInvalid input data!")
+        main()
+        
 def calculate_are_by_base_and_height():
-	str = input("Enter your base and height (example: 5 6): ")
-	base = input_validation(str, 0)
-	height = input_validation(str, 1)
-	# print("base", type(base))
-	# print("height", type(height))
-	# print(base, height)
-	area = (0.5 * base * height)
-	return print(f"Area is: {area:.0f} ")
+    str = input("Enter your base and height (example: 5 6): ")
+    base = input_validation(str, 0)
+    height = input_validation(str, 1)
+    # print("base", type(base))
+    # print("height", type(height))
+    # print(base, height)
+    area = (0.5 * base * height)
+    return print(f"Area is: {area:.0f} ")
 
 def calculate_are_by_sides_and_angle():
-	str = input("Enter 2 sides and angle between them (example: 15 8 30): ")
-	side_a = input_validation(str, 0)
-	side_b = input_validation(str, 1)
-	angle = input_validation(str, 2)
-	# print(side_a, side_b, angle)
-	area = 0.5 * side_a * side_b * math.sin(math.radians(angle))
-	return print(f"Area is: {area:.0f}")
+    str = input("Enter 2 sides and angle between them (example: 15 8 30): ")
+    side_a = input_validation(str, 0)
+    side_b = input_validation(str, 1)
+    angle = input_validation(str, 2)
+    # print(side_a, side_b, angle)
+    area = 0.5 * side_a * side_b * math.sin(math.radians(angle))
+    return print(f"Area is: {area:.0f}")
 
 def main():
-	menu_number = 0
-	print("Welcome to the triangle area calculation tool.")
-	while menu_number != 3:
-		menu_number = input('''
+    menu_number = 0
+    print("Welcome to the triangle area calculation tool.")
+    while menu_number != 3:
+        menu_number = input('''
 What would you like to do?\n 
 1. Calculate triangle area by base and height\n 
 2. Calculate triangle area by 2 sides and angle between them\n 
 3. Exit\n
 Enter menu item number: ''')
-
-		if menu_number == "1":
-			calculate_are_by_base_and_height()
-		elif menu_number == "2":
-			calculate_are_by_sides_and_angle()
-		elif menu_number == "3":
-			print("Goodbye!")
-			break
+        if menu_number == "1":
+            calculate_are_by_base_and_height()
+        elif menu_number == "2":
+            calculate_are_by_sides_and_angle()
+        elif menu_number == "3":
+            print("Goodbye!")
+            break
 
 if __name__ == "__main__":
-		main()
+    main()
