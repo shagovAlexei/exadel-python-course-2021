f07cd72d7cdba36b1156fc46c33b2ba0146883e8
shagovAlexei <shagovAlexei@gmail.com>
2021-09-17 15:41:34 +0300

    def input_validation

--- tasks/task02/triangle.py
+++ tasks/task02/triangle.py
@@ -22,41 +22,37 @@ Menu:
 Enter menu item number: 3
 Goodbye!
 '''
-
 import math
-
 
 def input_validation(input_string, item):
 		try:
 				value = abs(float(input_string.split()[item]))
+				if value == 0:
+					print("\nInvalid input data!")
+					main()
+				return value
 		except:
 				print("\nInvalid input data!")
-		return value
+				main()
 		
 def calculate_are_by_base_and_height():
-	x = input("Enter your base and height (example: 5 6): ")
-	parts = str(map(int, x.split()))
-	print(parts)
-	print(type(parts))
-	# parts = s.split(" ")
-	# base = parts[0]
-	base = input_validation(x, 0)
-	height = input_validation(x, 1)
-	print(base, height)
+	str = input("Enter your base and height (example: 5 6): ")
+	base = input_validation(str, 0)
+	height = input_validation(str, 1)
+	# print("base", type(base))
+	# print("height", type(height))
+	# print(base, height)
 	area = (0.5 * base * height)
-	return print(f"Area is: {area:.1f} ")
-
+	return print(f"Area is: {area:.0f} ")
 
 def calculate_are_by_sides_and_angle():
-	s = input("Enter 2 sides and angle between them (example: 15 8 30): ")
-	parts = s.split(" ")
-	side_a = int(parts[0])
-	side_b = parts[1]
-	angle = parts[2]
-	print(side_a, side_b, angle)
-	area = 0.5 * float(side_a) * float(side_b) * \
-						float(math.sin(math.radians(int(angle))))
-	return print(f"Area is: {area:.1f}")
+	str = input("Enter 2 sides and angle between them (example: 15 8 30): ")
+	side_a = input_validation(str, 0)
+	side_b = input_validation(str, 1)
+	angle = input_validation(str, 2)
+	# print(side_a, side_b, angle)
+	area = 0.5 * side_a * side_b * math.sin(math.radians(angle))
+	return print(f"Area is: {area:.0f}")
 
 def main():
 	print("Welcome to the triangle area calculation tool.")
@@ -75,7 +71,6 @@ Enter menu item number: ''')
 		elif menu_number == "3":
 			print("Goodbye!")
 			break
-
 
 if __name__ == "__main__":
 		main()
