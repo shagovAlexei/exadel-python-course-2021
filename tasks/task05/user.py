54e233a3a0b56d1f1c041a92ad7b378542d97c9b
shagovAlexei <shagovAlexei@gmail.com>
2021-09-26 22:39:22 +0300

    edit create_user()

--- tasks/task05/user.py
+++ tasks/task05/user.py
@@ -1,6 +1,8 @@
 # Shagov Aleksei
 
-def create_user():
-    pass
-
+def create_user(name, surname, age=42, **extra):
+    user, extra_dict = {}, {}
+    user['name'] = name
+    user['surname'] = surname
+    user['age'] = age
     
