5cfdd09e160fbf82c9b69047b85230c12a231c98
shagovAlexei <shagovAlexei@gmail.com>
2021-09-26 22:42:20 +0300

    edit create_user()

--- tasks/task05/user.py
+++ tasks/task05/user.py
@@ -5,4 +5,12 @@ def create_user(name, surname, age=42, **extra):
     user['name'] = name
     user['surname'] = surname
     user['age'] = age
-    
+    print(user)
+
+    for extra_key, extra_value in extra.items():
+        extra_dict[extra_key] = extra_value
+        print(extra_value)
+
+
+user = create_user("Bill", "Gates", age=65)
+print(user)
