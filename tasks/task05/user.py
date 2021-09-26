4514a1f9b6729c04625f9a4219e4db56bc1548c4
shagovAlexei <shagovAlexei@gmail.com>
2021-09-26 23:13:19 +0300

    edit create_user

--- tasks/task05/user.py
+++ tasks/task05/user.py
@@ -3,20 +3,13 @@
 # run
 # python -m pytest user.py -v
 
-def create_user(name, surname, age=42, **extra):
-    user, extra_dict = {}, {}
-    user['name'] = name
-    user['surname'] = surname
-    user['age'] = age
-    # print(user)
-    for extra_key, extra_value in extra.items():
-        extra_dict[extra_key] = extra_value
-        # print(extra_value)
-    user['extra'] = extra_dict
-    return user
-
-# user = create_user("Bill", "Gates", age=65)
-# print(user)
+def create_user(name: str, surname: str, age: int = 42, **kwargs):
+    return {
+        'name': name,
+        'surname': surname,
+        'age': age,
+        'extra': kwargs
+    }
 
 
 def test_two_params():
