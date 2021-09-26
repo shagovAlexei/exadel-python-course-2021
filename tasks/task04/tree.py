49a3673e882d614cf81b72dfd2312f44a52f86e5
shagovAlexei <shagovAlexei@gmail.com>
2021-09-26 19:55:34 +0300

    add collections_leaves

--- tasks/task04/tree.py
+++ tasks/task04/tree.py
@@ -1,3 +1,7 @@
+# Shagov Aleksei
+# run
+# python -m pytest tree.py -v
+
 tree = {
     "node1": {
         "node11": {
@@ -9,6 +13,10 @@ tree = {
     "node2": [7, 8, 9]
 }
 
-collections_leaves(tree) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-collections_leaves([1, 2, 3]) == [1, 2, 3]
+def test_with_dict_of_dicts():
+    assert collect_leaves(tree) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def collect_leaves(tree, list_final=[]):
+    pass
