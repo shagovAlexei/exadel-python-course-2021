314eac922824260f4655c99e800b8c1cccb75401
Aliaksei Shahau <ashahau@exadel.com>
2021-10-03 21:15:00 +0300

    add my_fn1

--- tasks/task06/decorators.py
+++ tasks/task06/decorators.py
@@ -6,3 +6,12 @@ def measure_elapsed_time(func):
         print(f"calling {func.__name__}")
 
     return wrapper
+
+
+@measure_elapsed_time
+def my_fn1(arg1, arg2):
+    time.sleep(0.5)
+    return arg1 + arg2
+
+
+print("my_fn1 result:", my_fn1(1, 2))
