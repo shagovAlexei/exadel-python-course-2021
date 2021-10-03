bc6b4d5f26cdda25c2ec49badde00c98d859cf24
Aliaksei Shahau <ashahau@exadel.com>
2021-10-03 21:54:02 +0300

    add class Order

--- tasks/task07/order_repository.py
+++ tasks/task07/order_repository.py
@@ -5,3 +5,12 @@ class Good:
         self.product_id = product_id
         self.name = name
         self.price = price
+
+
+class Order:
+    def __init__(self, order_id, order_date, client_id):
+        self.order_id = order_id
+        self.order_date = order_date
+        self.client_id = client_id
+        self._price = None
+        self._goods = []
