2367b6b92045d954a478110f853d9f54d15a95a9
Aliaksei Shahau <ashahau@exadel.com>
2021-10-03 21:13:13 +0300

    add measure_elapsed_time

--- tasks/task06/decorators.py
+++ tasks/task06/decorators.py
@@ -1,6 +1,8 @@
 import time
 
-@measure_elapsed_time
-def my_fn1(arg1, arg2):
-   time.sleep(0.5)
-   return arg1 + arg2
+
+def measure_elapsed_time(func):
+    def wrapper(*args, **kwargs):
+        print(f"calling {func.__name__}")
+
+    return wrapper
